6b6d5daa865635391686a0de0ad4c82c1dc72c38
Glimpz <abgabbard@gmail.com>
2020-12-07 16:14:14 -0700

    Adding more click handlers and creating score / time left containers

--- script.js
+++ script.js
@@ -1,60 +1,100 @@
-// Fucntions Needed
-// Start Game Function 
-// Correct Answer Function (Move to Next Question)
-// Wrong Answer Function (Move to Next Question but Subtract Time)
-// Timer Function (countdown from 3 minutes)
-// Do we want to randomly pull questions from an array... creat a function to select which question. Should this be random or do we just have it go through indexes?
-
 var startButton  = document.getElementById('start-btn')
-var checkButton = document.getElementById("check-btn")
+var checkAnswer = document.getElementById("check-btn")
 var questionContainer = document.getElementById("question-container")
+var timer = [] // Timer needs to start at 180 seconds - 
+var questionElement = document.getElementById("question")
+var answerElement = document.getElementById("answer-buttons")
+var timeLeft = document.getElementById("timeLeft")
+var scoreCount = document.getElementById("scoreCount")
+// Wiring up click handlers for each option (a, b, c , d) while putting them into a variable (aButton - dButton)
+
+var aButton = document.getElementById("aBtn")
+var bButton = document.getElementById("bBtn")
+var cButton = document.getElementById("cBtn")
+var dButton = document.getElementById("dBtn")
+
+let randomQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame)
-checkButton.addEventListener("click", checkAnswer)
+checkAnswer.addEventListener("click", checkAnswer)
 
 function startGame() {
-    console.log("started");
+    console.log("startGame-started");
     startButton.classList.add('hide')
+    randomQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0 
     questionContainer.classList.remove('hide')
-    checkAnswer
-    setNextQuestion
+    scoreCount.classList.remove('hide')
+    timeLeft.classList.remove('hide')
+    setNextQuestion()
+    // timeStart()
+    console.log("startGame-finished")
 }
 
 function checkAnswer() {
-console.log("checking answer")
-// This should check their selection to 
-}
-
+    console.log("checkAnswer-Started")
+    console.log("checkAnswer-Finished")
+} 
+//     if it is correct (DOES IT MATCH ANY FROM CORRECTANSWERARRAY)
+//   - RUN function setNextQuestion
+//     else     
+//     deduct 10 seconds from TIMER variable AND move to next question (WHILE) highlighting the correct answer (and waiting 1.5 seconds 
+//     before starting the function setNextQuestion.    
+// 
 function setNextQuestion(){
+    resetState()
+    console.log("setNextQuestion-started")
+    showQuestion(randomQuestions[currentQuestionIndex])
+    console.log("setNextQuestion-finished")
     // This will randomly check which questions they have already had, (no repeating) and set the next question
+    // We need this function to use inserttext innerhtml etc. to to put the question and options into the html and it will pull from randomQuestion[i]
 }
 
-function selecetAnswer() {
-    // Need to set an onclick event to set a value / select answer when the user actually selects it. The "check answer" button will compare correct answer to theirs.
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerElement.appendChild(button)
+    })
 }
 
-function timer(){
-
+function resetState() {
+    checkAnswer.classList.add('hide')
+    while (answerElement.firstChild) {
+        answerElement.removeChild(answerElement.firstChild)
+    }
 }
 
-randomQuestionSelection= 
+function selectAnswer(e){
+    selectedButton = e.target
+    correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+}
 
-// If we turn the below into an array - how do we tie the correct "choices" options for each .... As in, how do I make it so that Question 1Choices actually show up for Question 1 and not a different one, if we randomly loop through the array?
-Question1="What is JavaScript"
-Question2="Who is JavaScript"
-Question3="Where is JavaScript"
-Question4="When is JavaScript"
-Question5="Why is JavaScript"
+function timerStart(){
+    // We need to insert code here to start a countdown from 180 seconds. 
+}
 
-Question1Choices=[Option1A, Option1B, Option1C, Option1D]
-Question2Choices=[Option2A, Option2B, Option2C, Option2D]
-Question3Choices=[Option3A, Option3B, Option3C, Option3D]
-Question4Choices=[Option4A, Option4B, Option4C, Option4D]
-Question5Choices=[Option5A, Option5B, Option5C, Option5D]
-
-// There are two ways I think you could do this: randomly generate the answers in any order - and having the "correct" value tied to the answer or
-// We could create an array of correct - pre-determined (and ordered) answer choices and say if an answer "does not equal one of those" (in any question that is asked) deduct time from the quiz
-
-var CorrectAnswers=[Option1A, Option2C, Option3C, Option4B, Option5A]
-
+questions = [
+    {
+        question: "What is JavaScript",
+        answers: [
+            {text: "A Wonderful Language", correct: true},
+            {text: "Coffee", correct: false},
+            {text: "Hmmm Plz", correct: false},
+            {text: "I really really do not know", correct: false}
+        ]
+    }
+]
+// randomQuestion=Random choice (MathRandomMathFloor) from Questions Array.
+// // If we turn the below into an array - how do we tie the correct "choices" options for each .... As in, how do I make it so that Question 1Choices actually show up for Question 1 and not a different one, if we randomly loop through the array?
+// // There are two ways I think you could do this: randomly generate the answers in any order - and having the "correct" value tied to the answer or
+// // We could create an array of correct - pre-determined (and ordered) answer choices and say if an answer "does not equal one of those" (in any question that is asked) deduct time from the quiz
+// If the users answer matches any of these options - we want to setNextQuestion - and continue running the timer(no issue here)
+// var CorrectAnswerArray=[Option1A, Option2C, Option3C, Option4B, Option5A]
