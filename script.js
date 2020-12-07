daa2ee77e92f586d9262dbcad7c5f79286582f29
Glimpz <abgabbard@gmail.com>
2020-12-07 11:32:53 -0700

    Mapping out function ideas and setting up card style

---
+++ script.js
@@ -0,0 +1,7 @@
+// Fucntions Needed
+// Start Game Function 
+// Correct Answer Function (Move to Next Question)
+// Wrong Answer Function (Move to Next Question but Subtract Time)
+// Timer Function (countdown from 3 minutes)
+// Do we want to randomly pull questions from an array... creat a function to select which question. Should this be random or do we just have it go through indexes?
+
