ca396b7b7b3f88b3ffb10dd7ac927872b28b4177
Glimpz <abgabbard@gmail.com>
2020-12-07 17:03:32 -0700

    added functional timer to the page that counts down in seconds from 180 seconds (3 minutes)

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var startButton  = document.getElementById('start-btn')
+var startButton  = document.getElementById("start-btn")
 var checkAnswer = document.getElementById("check-btn")
 var questionContainer = document.getElementById("question-container")
-var timer = [] // Timer needs to start at 180 seconds - 
 var questionElement = document.getElementById("question")
 var answerElement = document.getElementById("answer-buttons")
 var timeLeft = document.getElementById("timeLeft")
+// For the Timer we need to Insert 180 second countdown / refreshing @ intervals 1 second
 var scoreCount = document.getElementById("scoreCount")
 // Wiring up click handlers for each option (a, b, c , d) while putting them into a variable (aButton - dButton)
 
+// May not need these: 
 var aButton = document.getElementById("aBtn")
 var bButton = document.getElementById("bBtn")
 var cButton = document.getElementById("cBtn")
@@ -33,21 +34,19 @@ function startGame() {
 
 function checkAnswer() {
     console.log("checkAnswer-Started")
+    // if user input equals any of the wrong answer (in wrong answer array) we will want to 
+    // run the function  wrongAnswer - which will deduct 10 seconds from the interval timer
     console.log("checkAnswer-Finished")
 } 
 //     if it is correct (DOES IT MATCH ANY FROM CORRECTANSWERARRAY)
 //   - RUN function setNextQuestion
-//     else     
-//     deduct 10 seconds from TIMER variable AND move to next question (WHILE) highlighting the correct answer (and waiting 1.5 seconds 
-//     before starting the function setNextQuestion.    
-// 
 function setNextQuestion(){
     resetState()
     console.log("setNextQuestion-started")
     showQuestion(randomQuestions[currentQuestionIndex])
     console.log("setNextQuestion-finished")
-    // This will randomly check which questions they have already had, (no repeating) and set the next question
-    // We need this function to use inserttext innerhtml etc. to to put the question and options into the html and it will pull from randomQuestion[i]
+// This will randomly check which questions they have already had, (no repeating) and set the next question
+// We need this function to use inserttext innerhtml etc. to to put the question and options into the html and it will pull from randomQuestion[i]
 }
 
 function showQuestion(question) {
@@ -71,14 +70,26 @@ function resetState() {
     }
 }
 
-function selectAnswer(e){
+function selectAnswer(e) {
     selectedButton = e.target
     correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
 }
 
-function timerStart(){
-    // We need to insert code here to start a countdown from 180 seconds. 
+function timerStart() {
+    //Insert code here to start a countdown from 180 seconds into the Insert into TIME LEFT Variable
+}
+
+function wrongAnswer() {
+    // IF user chose *any* wrong answer (correct: false) THEN
+
+    alert("Oops! WRONG!!!! 10 SECONDS OFF!")
+    // decrement total remaining time by 10 seconds.
+    
+}
+
+function increaseScore() {
+    // If Answer 
 }
 
 questions = [
@@ -92,6 +103,18 @@ questions = [
         ]
     }
 ]
+
+// Need to add a countdown timer to the page so we can deduct 10 seconds from it as soon as the 
+// user hits START
+
+var seconds = document.getElementById("countdown").textContent;
+var countdown = setInterval(function(){
+    seconds--;
+    (seconds == 1) ? document.getElementById("plural").textContent = "" : document.getElementById("plural").textContent = "s";
+    document.getElementById("countdown").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+},1000);
+
 // randomQuestion=Random choice (MathRandomMathFloor) from Questions Array.
 // // If we turn the below into an array - how do we tie the correct "choices" options for each .... As in, how do I make it so that Question 1Choices actually show up for Question 1 and not a different one, if we randomly loop through the array?
 // // There are two ways I think you could do this: randomly generate the answers in any order - and having the "correct" value tied to the answer or
